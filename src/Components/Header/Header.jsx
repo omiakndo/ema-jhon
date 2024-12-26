4734a2810ea78a37d53c7162fd2a7d1ac4861a4f
omiakando <shohalakndo@gmail.com>
2024-12-27 01:30:53 +0600

    header-1

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+
+import './Header.css'
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className="header">
+            <img src={logo} alt="" />
+           <div className='nav-item'>
+            <a href="/shop">SHOP</a>
+            <a href="/inventry">INVEMTORY</a>
+            <a href="/order">ORDER</a>
+            <a href="/login">LOGIN</a>
+           </div>
+        </nav>
+    );
+};
+
+export default Header;
